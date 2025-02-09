62ba996bbfae2ce13dcdcef85b0892a937fdf828
btuhany <114235184+btuhany@users.noreply.github.com>
2025-02-10 01:30:32 +0300

    +master view installer

---
+++ Experimental_MVC/Assets/Scripts/LoggerExample/Installers/MasterViewInstaller.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.LoggerExample.MVC.Entities.Counter;
+using Batuhan.Core.MVC.Unity;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts.LoggerExample.Installers
+{
+    public class MasterViewInstaller : MonoInstaller
+    {
+        [SerializeField] private BaseViewMonoBehaviour[] _views;
+        public override void InstallBindings()
+        {
+            for (int i = 0; i < _views.Length; i++)
+            {
+                var view = _views[i];
+                var viewType = view.GetType();
+                Container.Bind(viewType).FromInstance(view).AsSingle();
+            }
+            CounterEntityInstaller.Install(Container);
+        }
+    }
+}
+
