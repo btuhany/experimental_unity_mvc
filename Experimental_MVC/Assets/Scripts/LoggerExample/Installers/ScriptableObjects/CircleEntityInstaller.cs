517a4eff24d0e4694212de1b23c4b73941816720
btuhany <114235184+btuhany@users.noreply.github.com>
2025-02-10 18:28:25 +0300

    +refactor

---
+++ Experimental_MVC/Assets/Scripts/LoggerExample/Installers/ScriptableObjects/CircleEntityInstaller.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.LoggerExample.MVC.Entities.Circle;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts.LoggerExample.Installers.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "CircleEntityInstaller", menuName = "Scriptable Objects/Batuhan/MVC/Installers/CircleEntityInstaller")]
+    internal class CircleEntityInstaller : BaseEntityInstallerScriptableObject
+    {
+        [SerializeField] private CircleView _circleViewPrefab;
+        public override void InstallFrom(DiContainer container)
+        {
+            container.Bind<CircleView>().FromInstance(_circleViewPrefab).AsSingle();
+            container.Bind<CircleContext>().AsTransient();
+            container.BindFactory<CircleController, CircleController.Factory>()
+                .FromFactory<CircleFactory>();
+        }
+    }
+}
