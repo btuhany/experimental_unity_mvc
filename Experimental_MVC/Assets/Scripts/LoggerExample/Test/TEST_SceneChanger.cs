568a3c4c26a495fa024ffed212752fe73e182b4b
btuhany <114235184+btuhany@users.noreply.github.com>
2025-02-10 14:33:43 +0300

    +scene changer helper test

---
+++ Experimental_MVC/Assets/Scripts/LoggerExample/Test/TEST_SceneChanger.cs
@@ -0,0 +1,45 @@
+using Cysharp.Threading.Tasks;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TEST_SceneChanger : MonoBehaviour
+{
+    [SerializeField] private int _nextSceneIndex;
+    [SerializeField] private Button _button;
+    private void Awake()
+    {
+        _button.onClick.AddListener(OnButtonClick);
+    }
+    private void OnDestroy()
+    {
+        _button.onClick.RemoveListener(OnButtonClick);
+    }
+
+    private void OnButtonClick()
+    {
+        ChangeScene(_nextSceneIndex);
+    }
+
+    public void ChangeScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(1);
+    }
+    public async UniTaskVoid LoadSceneAsyncByIndex(int sceneIndex)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            if (asyncLoad.progress < 0.9f) // Yükleme %90'a ulaþana kadar bekle
+            {
+                Debug.Log($"Loading progress: {asyncLoad.progress * 100}%");
+            }
+            Debug.Log($"Loading progress: {asyncLoad.progress * 100}%");
+            await UniTask.Yield();
+        }
+    }
+
+}
