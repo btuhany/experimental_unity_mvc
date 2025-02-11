c122754738babe3ef45901b83c39cc0258dc13c7
btuhany <114235184+btuhany@users.noreply.github.com>
2025-02-12 02:04:14 +0300

    +command pattern

---
+++ Experimental_MVC/Assets/Scripts/Batuhan/CommandManager/CommandBinding.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Batuhan.CommandManager
+{
+    public interface ICommandBinding
+    {
+        Type CommandType { get; }
+        void Execute(ICommand command);
+        void Undo(ICommand command);
+
+    }
+    public class CommandBinding<TCommand> : ICommandBinding where TCommand : ICommand
+    {
+        public Type CommandType => typeof(TCommand);
+
+        private readonly Action<TCommand> _execute;
+        private readonly Action<TCommand> _undo;
+
+        public CommandBinding(Action<TCommand> execute, Action<TCommand> undo = null)
+        {
+            _execute = execute;
+            _undo = undo;
+        }
+
+        public void Execute(ICommand command)
+        {
+            if (command is TCommand typedCommand)
+            {
+                _execute?.Invoke(typedCommand);
+                command.OnExecute();
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid command type");
+            }
+        }
+
+        public void Undo(ICommand command)
+        {
+            if (command is TCommand typedCommand)
+            {
+                _undo?.Invoke(typedCommand);
+                command.OnUndo();
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid command type");
+            }
+        }
+        public bool ExecuteEquals(Action<TCommand> action) => _execute == action;
+    }
+}
---
+++ Experimental_MVC/Assets/Scripts/Batuhan/CommandManager/CommandManager.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Batuhan.CommandManager
+{
+    public class CommandManager : ICommandManager
+    {
+        private readonly Dictionary<Type, List<ICommandBinding>> _commandListeners = new();
+        private readonly Stack<ICommand> _executedCommands = new();
+
+        public void AddListener<TCommand>(CommandBinding<TCommand> binding) where TCommand : ICommand
+        {
+            Type commandType = typeof(TCommand);
+            if (!_commandListeners.ContainsKey(commandType))
+            {
+                _commandListeners[commandType] = new List<ICommandBinding>();
+            }
+            _commandListeners[commandType].Add(binding);
+        }
+
+        public void RemoveListener<TCommand>(CommandBinding<TCommand> binding) where TCommand : ICommand
+        {
+            Type commandType = typeof(TCommand);
+            if (_commandListeners.ContainsKey(commandType))
+            {
+                _commandListeners[commandType].Remove(binding);
+                if (_commandListeners[commandType].Count == 0)
+                {
+                    _commandListeners.Remove(commandType);
+                }
+            }
+        }
+        public void RemoveListenerFromExecuteCallback<TCommand>(Action<TCommand> executeCallback) where TCommand : ICommand
+        {
+            Type commandType = typeof(TCommand);
+            if (_commandListeners.ContainsKey(commandType))
+            {
+                var listenersCopy = _commandListeners[commandType].ToArray();
+                foreach (var binding in listenersCopy)
+                {
+                    if (binding is CommandBinding<TCommand> typedBinding && typedBinding.ExecuteEquals(executeCallback))
+                    {
+                        _commandListeners[commandType].Remove(binding);
+                        if (_commandListeners[commandType].Count == 0)
+                        {
+                            _commandListeners.Remove(commandType);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void ExecuteCommand(ICommand command)
+        {
+            Type commandType = command.GetType();
+            if (_commandListeners.ContainsKey(commandType))
+            {
+                foreach (var binding in _commandListeners[commandType])
+                {
+                    binding.Execute(command);
+                }
+            }
+            _executedCommands.Push(command);
+        }
+
+        public void UndoCommand(ICommand command)
+        {
+            Type commandType = command.GetType();
+            if (_commandListeners.ContainsKey(commandType))
+            {
+                foreach (var binding in _commandListeners[commandType])
+                {
+                    binding.Undo(command);
+                }
+            }
+        }
+        public void UndoLastExecutedCommand()
+        {
+            if (_executedCommands.Count > 0)
+            {
+                var lastCommand = _executedCommands.Pop();
+                Type commandType = lastCommand.GetType();
+                if (_commandListeners.ContainsKey(commandType))
+                {
+                    foreach (var binding in _commandListeners[commandType])
+                    {
+                        binding.Undo(lastCommand);
+                    }
+                }
+            }
+        }
+        public void Dispose()
+        {
+            _commandListeners.Clear();
+            _executedCommands.Clear();
+        }
+
+    }
+}
