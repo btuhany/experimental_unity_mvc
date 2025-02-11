3d172c9139cb5b3fdb3f265bc96e920dfdea3049
btuhany <114235184+btuhany@users.noreply.github.com>
2025-02-11 17:40:01 +0300

    +event interface

---
+++ Experimental_MVC/Assets/Scripts/Batuhan/EventBus/IEvent.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Assets.Scripts.Batuhan.EventBus
+{
+    public enum EventCategoryID
+    {
+        Global = 0,
+        UI = 1, //Notifies view and controller
+        Input = 2, //Notifies controller
+        View = 3, //Notifies view
+        Controller = 4,
+        Model = 5
+    }
+    public interface IEvent { }
+}
