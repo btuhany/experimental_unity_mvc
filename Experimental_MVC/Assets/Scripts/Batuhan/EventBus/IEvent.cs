4b0c79e7c9a2ebe5411aef6a162819040a41cf16
btuhany <114235184+btuhany@users.noreply.github.com>
2025-02-11 20:38:02 +0300

    +event category folder

--- Experimental_MVC/Assets/Scripts/Batuhan/EventBus/IEvent.cs
+++ Experimental_MVC/Assets/Scripts/Batuhan/EventBus/IEvent.cs
@@ -1,19 +1,5 @@
 ﻿namespace Batuhan.EventBus
 {
-    //TODOBY flags
-    public enum EventCategoryID
-    {
-        Global = 0,
-        Service = 1, 
-        Input = 2,
-        View = 3,
-        Controller = 4,
-        Model = 5
-    }
-    public interface IEventCategory
-    {
-        public EventCategoryID ID { get; }
-    }
     public interface IEvent 
     {
         public EventCategoryID CategoryID { get; }
---
+++ Experimental_MVC/Assets/Scripts/Batuhan/EventBus/IEventCategory.cs
@@ -0,0 +1,17 @@
+﻿namespace Batuhan.EventBus
+{
+    //TODOBY flags
+    public enum EventCategoryID
+    {
+        Global = 0,
+        Service = 1,
+        Input = 2,
+        View = 3,
+        Controller = 4,
+        Model = 5
+    }
+    public interface IEventCategory
+    {
+        public EventCategoryID ID { get; }
+    }
+}
