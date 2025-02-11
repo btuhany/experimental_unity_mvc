13cd251c7576fc05a6ad008ed7a8515448079448
btuhany <114235184+btuhany@users.noreply.github.com>
2025-02-11 17:39:51 +0300

    +event binding wrrapper

---
+++ Experimental_MVC/Assets/Scripts/Batuhan/EventBus/EventBinding.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Assets.Scripts.Batuhan.EventBus
+{
+    public class EventBinding<T> where T : IEvent
+    {
+        public Action<T> OnEvent { get; private set; }
+        public EventBinding(Action<T> onEvent) => OnEvent = onEvent;
+
+        //public bool Equals(EventBinding<T> other)
+        //{
+        //    if (other is null)
+        //        return false;
+        //    return other.OnEvent == OnEvent;
+        //}
+        //public override bool Equals(object obj) => Equals(obj as EventBinding<T>);
+        //public override int GetHashCode() => OnEvent?.GetHashCode() ?? 0;
+    }
+}
