15a5215399f4d97899ed9ff6461aeed9e8e5c7ed
btuhany <114235184+btuhany@users.noreply.github.com>
2025-02-12 16:26:26 +0300

    +updated icommandmanager

--- Experimental_MVC/Assets/Scripts/Batuhan/CommandManager/ICommandManager.cs
+++ Experimental_MVC/Assets/Scripts/Batuhan/CommandManager/ICommandManager.cs
@@ -5,7 +5,6 @@ namespace Batuhan.CommandManager
     public interface ICommandManager
     {
         void AddListener<TCommand>(CommandBinding<TCommand> binding) where TCommand : ICommand;
-
         void RemoveListener<TCommand>(CommandBinding<TCommand> binding) where TCommand : ICommand;
         void ExecuteCommand(ICommand command);
     }
