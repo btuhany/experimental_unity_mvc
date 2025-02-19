9968bd5781237b09b9d0f0576a8317803a1352a1
btuhany <114235184+btuhany@users.noreply.github.com>
2025-02-19 18:13:44 +0300

    +time ticker installer

---
+++ Experimental_MVC/Assets/Scripts/TimeCounter/DI_Installers/ScriptableObjects/TimeTickerInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Batuhan.MVC.Core;
+using Batuhan.MVC.UnityComponents.Zenject;
+using TimeCounter.Data;
+using TimeCounter.Entities.Counter;
+using UnityEngine;
+using Zenject;
+
+namespace TimeCounter.Installers
+{
+    [CreateAssetMenu(fileName = "TimeTickerInstaller", menuName = "Scriptable Objects/Batuhan/MVC/Installers/TimeTickerInstaller")]
+    internal class TimeTickerInstaller : BaseEntityInstallerSO
+    {
+        [SerializeField] private CounterModelDataSO _modelDataSO;
+        public override void InstallFrom(DiContainer container)
+        {
+            container.Bind<ICounterContext>().To<CounterContext>().AsTransient();
+            container.Bind<ICounterModel>().To<CounterModel>().AsTransient();
+            container.Bind<ILifeCycleHandler>().To<CounterController>().AsTransient();
+            container.Bind<CounterModelDataSO>().FromScriptableObject(_modelDataSO).AsTransient();
+        }
+    }
+}
