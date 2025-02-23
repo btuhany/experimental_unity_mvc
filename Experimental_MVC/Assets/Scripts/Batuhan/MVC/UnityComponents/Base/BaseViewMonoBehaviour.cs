d7472af7b073fc1762f30eefdefc4e81b8deb34f
btuhany <114235184+btuhany@users.noreply.github.com>
2025-02-23 17:30:38 +0300

    +base singleton view mono behaviour

--- Experimental_MVC/Assets/Scripts/Batuhan/MVC/UnityComponents/Base/BaseViewMonoBehaviour.cs
+++ Experimental_MVC/Assets/Scripts/Batuhan/MVC/UnityComponents/Base/BaseViewMonoBehaviour.cs
@@ -8,9 +8,37 @@ namespace Batuhan.MVC.UnityComponents.Base
     {
         public abstract Type ContractTypeToBind { get; }
 
-        public virtual void Dispose() 
+        public virtual void Dispose()
         {
             Destroy(gameObject);
+        }
+    }
+    public abstract class BaseSingletonViewMonoBehaviour<T> : BaseViewMonoBehaviour where T : BaseSingletonViewMonoBehaviour<T>
+    {
+        private static T _instance;
+        protected bool TrySetSingletonAsDDOL(bool setParentNullIfExist = false)
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(gameObject);
+
+                return false;
+            }
+            else if (_instance == null)
+            {
+                _instance = (T) this;
+
+                if (setParentNullIfExist || transform.parent == null)
+                {
+                    transform.SetParent(null);
+                    DontDestroyOnLoad(gameObject);
+                }
+                else
+                {
+                    Debug.LogWarning($"Singleton view {nameof(T)} has a gameObject parent! It cannot be marked as DDOL!");
+                }
+            }
+            return true;
         }
     }
 }
