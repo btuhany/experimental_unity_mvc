67f50a5641356d4a739d62289fea62f886361dff
btuhany <114235184+btuhany@users.noreply.github.com>
2025-03-13 15:34:32 +0300

    +input listens game event

--- Experimental_MVC/Assets/Scripts/Snake/Entities/InputReader/InputReaderContext.cs
+++ Experimental_MVC/Assets/Scripts/Snake/Entities/InputReader/InputReaderContext.cs
@@ -1,13 +1,17 @@
+using Batuhan.EventBus;
 using Batuhan.MVC.Core;
+using SnakeExample.Events;
+using Zenject;
 
 namespace SnakeExample.Entities.InputReader
 {
     public interface IInputReaderContext : IContext
     {
-
+        IEventBus<GameEvent> EventBus { get; }
     }
     public class InputReaderContext : IInputReaderContext
     {
+        [Inject] IEventBus<GameEvent> EventBus { get; }
         public void Dispose()
         {
         }
--- Experimental_MVC/Assets/Scripts/Snake/Entities/InputReader/InputReaderController.cs
+++ Experimental_MVC/Assets/Scripts/Snake/Entities/InputReader/InputReaderController.cs
@@ -1,16 +1,20 @@
 using Batuhan.MVC.Base;
 using Batuhan.MVC.Core;
 using ExperimentalMVC.SnakeExample;
+using SnakeExample.Events;
+using System;
 using UnityEngine.InputSystem;
 
 namespace SnakeExample.Entities.InputReader
 {
     public class InputReaderController : BaseController<IInputReaderContext>, ISceneLifeCycleManaged, SnakeExampleInputActions.ISnakeSceneActions
     {
+        private bool _canRead;
         private SnakeExampleInputActions _inputActions;
         private SnakeExampleInputActions.SnakeSceneActions _snakeSceneActions;
         public InputReaderController(IInputReaderContext context) : base(context)
         {
+            _canRead = false;
         }
 
         public void OnAwakeCallback()
@@ -18,14 +22,20 @@ namespace SnakeExample.Entities.InputReader
             _inputActions = new SnakeExampleInputActions();
             _snakeSceneActions = _inputActions.SnakeScene;
             _snakeSceneActions.AddCallbacks(this);
-
             _snakeSceneActions.Enable();
+            _context.EventBus.Subscribe<GameStateChanged>(OnGameStateChanged);
+        }
+
+        private void OnGameStateChanged(GameStateChanged changed)
+        {
+            _canRead = changed.NewState == GameManager.GameState.Started;
         }
 
         public void OnDestroyCallback()
         {
             _snakeSceneActions.Disable();
             _inputActions.Dispose();
+            _context.EventBus.Unsubscribe<GameStateChanged>(OnGameStateChanged);
         }
 
         public void OnMoveUp(InputAction.CallbackContext context)
