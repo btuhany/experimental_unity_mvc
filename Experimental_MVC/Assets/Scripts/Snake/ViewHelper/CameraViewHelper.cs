d3d2f99ecb64228adf69ab0e3f9665f27b069de8
btuhany <114235184+btuhany@users.noreply.github.com>
2025-03-17 13:10:49 +0300

    +camera view helper

---
+++ Experimental_MVC/Assets/Scripts/Snake/ViewHelper/CameraViewHelper.cs
@@ -0,0 +1,20 @@
+using Batuhan.EventBus;
+using SnakeExample.Events;
+using SnakeExample.Grid;
+using UnityEngine;
+using Zenject;
+
+namespace SnakeExample.View
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraViewHelper : MonoBehaviour
+    {
+        [Inject] private GridManager _gridManager;
+
+        private void Start()
+        {
+            var gridPos = _gridManager.Grid.GetGridCenterWorldPosition();
+            transform.position = new Vector3(gridPos.x, gridPos.y, transform.position.z);
+        }
+    }
+}
